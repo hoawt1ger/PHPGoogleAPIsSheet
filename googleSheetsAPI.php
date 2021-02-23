01f974adb91d30cb0efee1c74b80cf9604f0ea0d
hoawt1ger <51823003+hoawt1ger@users.noreply.github.com>
2021-02-23 15:21:31 +0700

    Update googleSheetsAPI.php

--- googleSheetsAPI.php
+++ googleSheetsAPI.php
@@ -33,13 +33,13 @@
 	$client = new \Google_Client();
     $client->setApplicationName('Google Sheets API PHP Quickstart');
     $client->setScopes(\Google_Service_Sheets::SPREADSHEETS);
-    $client->setAuthConfig(__DIR__.'/amiable-octane-272311-e2e44b0f4852.json');
+    $client->setAuthConfig(__DIR__.'/promushroomservices.json');
     $client->setAccessType('offline');
     // $client->setPrompt('select_account consent');
 
     $service = new \Google_Service_Sheets($client);
 
-    $spreadsheetId = "GOOGLE-SHEETS-ID";
+    $spreadsheetId = "1GqsV4QhyamvjoUBcfTWJLo35qMI3OX1aMkU3HHbhko4";
 
     // updateData($spreadsheetId,$service);
     insertData($spreadsheetId,$service,$displayName);
